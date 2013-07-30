7d9876f0c41c7a656d6474c43412343b85559aaa
Piotr Czarnecki <pioczarn@gmail.com>
2013-07-30 22:30:05 +0100

    Change requires

--- lib/bitformat.rb
+++ lib/bitformat.rb
@@ -1,7 +1,6 @@
-require 'stringio'
-
 require_relative 'bitformat/field'
 require_relative 'bitformat/stream'
 require_relative 'bitformat/array'
 require_relative 'bitformat/numeric'
 require_relative 'bitformat/string'
+require_relative 'bitformat/bitfield'
--- lib/bitformat/field.rb
+++ lib/bitformat/field.rb
@@ -1,3 +1,4 @@
+require 'stringio'
 require 'set'
 
 module BitFormat
